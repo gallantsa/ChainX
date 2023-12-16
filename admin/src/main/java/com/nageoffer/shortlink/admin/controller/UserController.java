f1bbe592141e96dbf735416dc19a69c814c798b1
gallantsa <gallantsa@163.com>
2023-12-16 19:48:19 +0800

    开发短链接后管系统相关功能

--- admin/src/main/java/com/nageoffer/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.nageoffer.shortlink.admin.controller;
 
+import com.nageoffer.shortlink.admin.common.convention.result.Result;
+import com.nageoffer.shortlink.admin.common.convention.result.Results;
 import com.nageoffer.shortlink.admin.dto.resp.UserRespDTO;
 import com.nageoffer.shortlink.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -20,7 +22,7 @@ public class UserController {
      * 根据用户名查询用户信息
      */
     @GetMapping("/api/shortlink/v1/user/{username}")
-    public UserRespDTO getUserByUsername(@PathVariable("username") String username) {
-        return userService.getUserByUsername(username);
+    public Result<UserRespDTO> getUserByUsername(@PathVariable("username") String username) {
+        return Results.success(userService.getUserByUsername(username));
     }
 }
