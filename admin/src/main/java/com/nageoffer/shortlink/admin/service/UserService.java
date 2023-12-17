b15b3423124eed3e3b057c7baac2fa08a38f042c
gallantsa <gallantsa@163.com>
2023-12-17 14:59:54 +0800

    开发短链接后管系统用户注册相关功能

--- admin/src/main/java/com/nageoffer/shortlink/admin/service/UserService.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/service/UserService.java
@@ -2,6 +2,7 @@ package com.nageoffer.shortlink.admin.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.nageoffer.shortlink.admin.dao.entity.UserDO;
+import com.nageoffer.shortlink.admin.dto.req.UserRegisterReqDTO;
 import com.nageoffer.shortlink.admin.dto.resp.UserRespDTO;
 
 /**
@@ -15,4 +16,17 @@ public interface UserService extends IService<UserDO> {
      * @return  用户返回实体
      */
     UserRespDTO getUserByUsername(String username);
+
+    /**
+     * 查询用户名是否可用
+     * @param username  用户名
+     * @return  用户名可用返回 true，否则返回 false
+     */
+    Boolean hasUsername(String username);
+
+    /**
+     * 用户注册
+     * @param requestParam  用户注册请求参数
+     */
+    void register(UserRegisterReqDTO requestParam);
 }
