455b77380da7d4d9751ff8a4117a915b751e9c49
gallantsa <gallantsa@163.com>
2023-12-18 15:19:44 +0800

    feature: 开发短链接后管系统用户修改功能

--- admin/src/main/java/com/nageoffer/shortlink/admin/config/MyMetaObjectHandler.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/config/MyMetaObjectHandler.java
@@ -4,7 +4,6 @@ import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import org.apache.ibatis.reflection.MetaObject;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDateTime;
 import java.util.Date;
 
 @Component
@@ -19,6 +18,6 @@ public class MyMetaObjectHandler implements MetaObjectHandler {
 
     @Override
     public void updateFill(MetaObject metaObject) {
-        this.strictUpdateFill(metaObject, "updateTime", LocalDateTime::now, LocalDateTime.class);
+        this.strictUpdateFill(metaObject, "updateTime", Date::new, Date.class);
     }
 }
