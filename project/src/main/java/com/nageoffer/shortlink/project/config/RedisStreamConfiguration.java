77a2022ea1ae316c632e021f6b1e5ff73b4c8ff7
gallantsa <gallantsa@163.com>
2024-03-19 08:46:17 +0800

    review: 为Redis Stream 消息队列配置添加注释

--- project/src/main/java/com/nageoffer/shortlink/project/config/RedisStreamConfiguration.java
+++ project/src/main/java/com/nageoffer/shortlink/project/config/RedisStreamConfiguration.java
@@ -31,10 +31,19 @@ public class RedisStreamConfiguration {
     private final RedisConnectionFactory redisConnectionFactory;
     private final ShortLinkStatsSaveConsumer shortLinkStatsSaveConsumer;
 
+
+    /**
+     * 创建用于异步消息消费的线程池 Bean
+     *
+     * @return ExecutorService 实例
+     */
     @Bean
     public ExecutorService asyncStreamConsumer() {
+        // 用于生成线程名称的序号, 确保线程名称唯一
         AtomicInteger index = new AtomicInteger();
+        // 获得当前机器的 CPU 核心数
         int processors = Runtime.getRuntime().availableProcessors();
+        // 创建线程池, 这里没有设置拒绝策略, 使用默认的拒绝策略
         return new ThreadPoolExecutor(processors,
                 processors + processors >> 1,
                 60,
@@ -49,8 +58,15 @@ public class RedisStreamConfiguration {
         );
     }
 
+    /**
+     * 创建用于监听 Redis Stream 消息的容器 Bean
+     *
+     * @param asyncStreamConsumer 异步消息消费者线程池
+     * @return StreamMessageListenerContainer 实例
+     */
     @Bean(initMethod = "start", destroyMethod = "stop")
     public StreamMessageListenerContainer<String, MapRecord<String, String, String>> streamMessageListenerContainer(ExecutorService asyncStreamConsumer) {
+        // 配置监听容器选项
         StreamMessageListenerContainer.StreamMessageListenerContainerOptions<String, MapRecord<String, String, String>> options =
                 StreamMessageListenerContainer.StreamMessageListenerContainerOptions
                         .builder()
@@ -61,8 +77,11 @@ public class RedisStreamConfiguration {
                         // 如果没有拉取到消息，需要阻塞的时间。不能大于 ${spring.data.redis.timeout}，否则会超时
                         .pollTimeout(Duration.ofSeconds(3))
                         .build();
+        // 创建 StreamMessageListenerContainer
         StreamMessageListenerContainer<String, MapRecord<String, String, String>> streamMessageListenerContainer =
                 StreamMessageListenerContainer.create(redisConnectionFactory, options);
+
+        // // 设置消息的接收规则
         streamMessageListenerContainer.receiveAutoAck(Consumer.from(SHORT_LINK_STATS_STREAM_GROUP_KEY, "stats-consumer"),
                 StreamOffset.create(SHORT_LINK_STATS_STREAM_TOPIC_KEY, ReadOffset.lastConsumed()), shortLinkStatsSaveConsumer);
         return streamMessageListenerContainer;
