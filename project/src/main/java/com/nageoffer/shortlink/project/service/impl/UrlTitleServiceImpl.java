4d96f9e291bb7a28ace7c9cc6ce97f7aec35f573
gallantsa <gallantsa@163.com>
2024-03-19 12:32:44 +0800

    feature: 为根据 URL 获取对应网站的标题功能添加注释

--- project/src/main/java/com/nageoffer/shortlink/project/service/impl/UrlTitleServiceImpl.java
+++ project/src/main/java/com/nageoffer/shortlink/project/service/impl/UrlTitleServiceImpl.java
@@ -18,15 +18,23 @@ public class UrlTitleServiceImpl implements UrlTitleService {
     @SneakyThrows
     @Override
     public String getTitleByUrl(String url) {
+        // 使用URL类将给定的URL转换为URL对象
         URL targetUrl = new URL(url);
+        // 打开一个HTTP连接到该URL
         HttpURLConnection connection = (HttpURLConnection) targetUrl.openConnection();
+        // 设置HTTP请求方法为GET，并连接到该URL
         connection.setRequestMethod("GET");
         connection.connect();
+        // 获取HTTP响应码
         int responseCode = connection.getResponseCode();
+        // 如果响应码为200，表示请求成功
         if (responseCode == HttpURLConnection.HTTP_OK) {
+            // 使用Jsoup连接到给定的URL并获取网页文档
             Document document = Jsoup.connect(url).get();
+            // 返回网页标题
             return document.title();
         }
+        // 如果请求失败，返回错误信息
         return "Error while fetching title.";
     }
 }
